1a570eca3fb59e193831b7dbff8a79322edb17d4
Robin P <palotai.robin@gmail.com>
2015-11-12 23:03:50 +0100

    Added more programs.

--- common.nix
+++ common.nix
@@ -11,25 +11,47 @@
   # Set your time zone.
   time.timeZone = "Europe/Budapest";
 
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     curl wget
-    gnupg
+    gnupg gnupg1compat
     fish tmux
-    iftop nethogs
+    iftop nethogs htop
     ghc cabal-install stack
     vim vimPlugins.ghc-mod-vim
-    git gitAndTools.git-annex
+    git gitAndTools.git-annex gitAndTools.gitRemoteGcrypt
+    chromium
+    skype kde4.konversation
+    vlc mplayer
+    unrar
   ];
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbVariant = "dvp";
-  services.xserver.xkbOptions = "eurosign:e";
+  security.polkit = {
+    enable = true;
+    extraConfig =
+      ''
+          /* Log authorization checks. */
+          polkit.addRule(function(action, subject) {
+            polkit.log("subject " +  subject + " is attempting action " + action);
+          });
+      '';
+  };
 
-  # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.kdm.enable = true;
-  services.xserver.desktopManager.kde4.enable = true;
+  programs.ssh.startAgent = true;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "dvp";
+    xkbOptions = "eurosign:e";
+    displayManager.kdm.enable = true;
+    desktopManager.kde4.enable = true;
+    synaptics = {
+      enable = true;
+      vertTwoFingerScroll = true;
+    };
+  };
   
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "15.09";
